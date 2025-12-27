0f4c4e12bb63e867840c05d2bf117ccdc653c50a
shri030306 <shrilakshmigs03@gmail.com>
2025-12-27 12:37:09 +0530

    save

--- student.py
+++ student.py
@@ -1,14 +1,15 @@
-def student_details(usn,name,division,age):
-    result=(
+def student_details(usn, name, division, age):
+    result = (
         f"USN:{usn}\n"
         f"Name:{name}\n"
         f"Division:{division}\n"
         f"Age:{age}"
     )
+    return result   
 
 if __name__ == "__main__":
-    usn="01fe24bca314"
-    name="shree"
-    division="E"
-    age=19
-    print(student_details(usn,name,division,age))
+    usn = "01fe24bca314"
+    name = "shree"
+    division = "E"
+    age = 19
+    print(student_details(usn, name, division, age))
