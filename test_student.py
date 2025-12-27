6ad6d8d0cd3a1ae8ff1c7f6e027d1f1afe555a41
shri030306 <shrilakshmigs03@gmail.com>
2025-12-27 12:33:19 +0530

    save

--- test_student.py
+++ test_student.py
@@ -1,12 +1,13 @@
+# test_student.py
+
 from student import student_details
 
 def test_student_details():
-    expected_output=(
+    expected_output = (
         "USN:01fe24bca314\n"
         "Name:shree\n"
         "Division:E\n"
         "Age:19"
-
     )
-    assert student_details("01fe24bca314","shree","E",19)==expected_output
+    assert student_details("01fe24bca314", "shree", "E", 19) == expected_output
     
